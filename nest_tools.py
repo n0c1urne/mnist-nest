c0046dcc30cc4c6af354b50e29ab1f865992cbaa
n0c1urne <mail@ingmar.net>
2019-01-31 16:00:42 +0100

    debug output

--- nest_tools.py
+++ nest_tools.py
@@ -182,14 +182,23 @@ class Network:
         nest.SetStatus(detector,'n_events',0)
 
     def snapshot_connectivity_matrix(self):
+        print("Quering nest for connections")
         local_connections = nest.GetConnections(self.excitatory_neurons, self.excitatory_neurons)
+        
+        print("Quering nest for sources")
         sources = np.array(nest.GetStatus(local_connections, 'source'))
+
+        print("Quering nest for targets")
         targets = np.array(nest.GetStatus(local_connections, 'target'))
 
+        print("Creating matrix")
         matrix = np.zeros((params.NE,params.NE))
+        
+        print("Iterating sources")
         for ii in np.arange(sources.shape[0]):
             matrix[targets[ii]-1,sources[ii]-1] += 1
         
+        print("Returning matrix")
         return matrix
     
     def snapshot_connectivity(self):
