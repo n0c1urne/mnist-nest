f0104aaa63ab40ec163e1c690aa6292444449758
n0c1urne <mail@ingmar.net>
2019-02-04 13:38:51 +0100

    mnist plasticity with teacher signal

--- 6_mnist_with_plasticity/mnist_plasticity.py
+++ 6_mnist_with_plasticity/mnist_plasticity.py
@@ -69,6 +69,14 @@ with gzip.open('data/snapshots'+str(nest.Rank()), 'wb') as f:
         for j, rate in enumerate(input_rates):
             network.set_rate([j+1], rate)
 
+        teacher_stim_index = 3000+y_train[t]*100
+
+        for j in range(3000,  4000):
+            network.set_rate([j+1], params.rate)
+
+        for j in range(teacher_stim_index,  teacher_stim_index+100):
+            network.set_rate([j+1], 1.1 * params.rate)
+
         print("Nr.", t, "Digit", y_train[t])
         nest.Simulate(1000)
 
